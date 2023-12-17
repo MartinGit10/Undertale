ac2fd3563e4128dc3021ff98a3e5eb5d56e0b95a
MartinGit10 <147890378+MartinGit10@users.noreply.github.com>
2023-12-17 12:59:51 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 // game.js
 let enemyHealth = 100;
+let enemySmallDirection = 1; // 1 para derecha, -1 para izquierda
+const enemySmallSpeed = 2; // Ajusta la velocidad según sea necesario
 
 document.getElementById('attack-btn').addEventListener('click', () => {
     const damage = Math.floor(Math.random() * 10) + 1; // Daño aleatorio entre 1 y 10
@@ -17,3 +19,22 @@ function resetBattle() {
     enemyHealth = 100;
     document.getElementById('enemy-health').innerText = `Enemy Health: ${enemyHealth}`;
 }
+
+function moveEnemySmall() {
+    const enemySmall = document.getElementById('enemy-small');
+    const enemySmallPosition = parseInt(getComputedStyle(enemySmall).left);
+    
+    // Cambiar la dirección cuando alcanza los bordes
+    if (enemySmallPosition <= 0 || enemySmallPosition >= 180) {
+        enemySmallDirection *= -1;
+    }
+
+    // Mover el cuadrado rojo
+    enemySmall.style.left = (enemySmallPosition + enemySmallSpeed * enemySmallDirection) + 'px';
+
+    // Llamada recursiva para animación continua
+    requestAnimationFrame(moveEnemySmall);
+}
+
+// Iniciar la animación cuando se carga la página
+moveEnemySmall();
