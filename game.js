034641f5c45dcde2c99d7f449d548b8901746522
MartinGit10 <147890378+MartinGit10@users.noreply.github.com>
2023-12-17 12:39:12 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,2 +1,19 @@
 // game.js
-console.log("Hello from game.js!");
+let enemyHealth = 100;
+
+document.getElementById('attack-btn').addEventListener('click', () => {
+    const damage = Math.floor(Math.random() * 10) + 1; // Daño aleatorio entre 1 y 10
+    enemyHealth -= damage;
+
+    if (enemyHealth <= 0) {
+        alert('¡Has derrotado al enemigo!');
+        resetBattle();
+    } else {
+        document.getElementById('enemy-health').innerText = `Enemy Health: ${enemyHealth}`;
+    }
+});
+
+function resetBattle() {
+    enemyHealth = 100;
+    document.getElementById('enemy-health').innerText = `Enemy Health: ${enemyHealth}`;
+}
